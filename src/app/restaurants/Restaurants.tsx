64a0f69a762dba23e76e5dd862c19a81f2bbc985
HiroshiOyamada <h5y1m141+github@gmail.com>
2023-07-01 07:54:59 +0900

    レストランのサブコレクション配下の情報を再帰的に取得して画面表示できるように修正

--- src/app/restaurants/Restaurants.tsx
+++ src/app/restaurants/Restaurants.tsx
@@ -1,41 +1,83 @@
 'use client'
 
-import { getDocs, collection, getFirestore, query } from 'firebase/firestore'
+import {
+  getDocs,
+  getDoc,
+  collection,
+  getFirestore,
+  query,
+} from 'firebase/firestore'
+import { useRouter } from 'next/navigation'
 import React, { useEffect, useState, FC } from 'react'
 import { useAuthContext } from '@/src/app/context/auth'
 import { firebaseApp } from '@/src/app/firebase'
 
-type dataType = {
+type Restaurant = {
+  id: string
   name: string
+  phone: string
+  prefecture: string
 }
 export const Restaurants: FC = () => {
+  const router = useRouter()
   const authContext = useAuthContext()
-  const [data, setData] = useState<dataType[]>()
+  const [restaurants, setRestaurants] = useState<Restaurant[]>()
+
   useEffect(() => {
     ;(async () => {
       const collectionName = 'restaurants'
 
       if (authContext.user?.uid) {
         const db = getFirestore(firebaseApp)
-        const restaurantsQuery = query(collection(db, collectionName))
-        const querySnapshot = await getDocs(restaurantsQuery)
-        const docSnap = querySnapshot.docs.map((doc) => doc.data())
 
-        if (docSnap.length > 0) setData(docSnap as unknown as dataType[])
+        const restaurantCollection = collection(db, collectionName)
+        const restaurantSnapshot = await getDocs(restaurantCollection)
+
+        const loadedRestaurants: Restaurant[] = []
+        for (const doc of restaurantSnapshot.docs) {
+          const name = doc.get('name')
+          const phone = doc.get('phone')
+          const addressCollection = collection(doc.ref, 'address')
+          const addressSnapshot = await getDocs(addressCollection)
+          const prefecture = addressSnapshot.docs[0].get('prefecture')
+
+          loadedRestaurants.push({
+            id: doc.id,
+            name,
+            phone,
+            prefecture,
+          })
+        }
+        setRestaurants(loadedRestaurants)
       }
     })()
-  }, [authContext.user?.uid])
+  }, [authContext.user?.uid, router])
 
   return (
     <>
-      <h1>Restaurants</h1>
-      <h2>取得件数：{data?.length}</h2>
-      {data && data?.length > 0 && (
-        <>
-          {data.map((item) => {
-            return <div key={item.name}>{item.name}</div>
-          })}
-        </>
+      {restaurants && restaurants?.length > 0 && (
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>店舗名</th>
+              <th>店舗連絡先</th>
+              <th>住所</th>
+            </tr>
+          </thead>
+          <tbody>
+            {restaurants.map((restaurant) => {
+              return (
+                <tr key={restaurant.id}>
+                  <td>{restaurant.id}</td>
+                  <td>{restaurant.name}</td>
+                  <td>{restaurant.phone}</td>
+                  <td>{restaurant.prefecture}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
       )}
     </>
   )
