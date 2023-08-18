ccba04359e881ca1fa61921c3fc04b0b38a167a4
HiroshiOyamada <h5y1m141+github@gmail.com>
2023-08-18 12:20:36 +0900

    cURLのPOSTでseatsに対してレコード登録できるように実装

--- rest_base_sample/src/seats/seats.service.ts
+++ rest_base_sample/src/seats/seats.service.ts
@@ -2,6 +2,14 @@ import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
 import { Seat } from './seats.entity'
+import { Restaurant } from '../restaurants/restaurants.entity'
+
+export type CreateSeatType = {
+  number_of_seats: number
+  start_at: string
+  end_at: string
+  restaurant_id: number
+}
 
 @Injectable()
 export class SeatsService {
@@ -17,6 +25,19 @@ export class SeatsService {
   }
 
   findOne(id: number): Promise<Seat | null> {
-    return this.seatRepository.findOneBy({ id })
+    return this.seatRepository.findOne({
+      where: { id },
+      relations: ['restaurant'],
+    })
+  }
+
+  async create(seat: CreateSeatType): Promise<Seat> {
+    return this.seatRepository.save({
+      ...seat,
+      start_at: new Date(seat.start_at),
+      end_at: new Date(seat.end_at),
+      created_at: new Date(),
+      updated_at: new Date(),
+    })
   }
 }
