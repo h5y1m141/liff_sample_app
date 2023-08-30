e1e1d4c3860e9a9dd0f41d88017f97337507a7fe
HiroshiOyamada <h5y1m141+github@gmail.com>
2023-08-30 12:20:52 +0900

    管理画面の予約一覧取得ロジックをhooksとして切り出し

--- src/app/admin/operation_for_reservations/OperationForReservationList.tsx
+++ src/app/admin/operation_for_reservations/OperationForReservationList.tsx
@@ -1,60 +1,12 @@
 'use client'
 
-import {
-  collection,
-  getFirestore,
-  where,
-  query,
-  onSnapshot,
-  orderBy,
-  limit,
-} from 'firebase/firestore'
-
 import { useRouter } from 'next/navigation'
-import React, { useEffect, useState, FC, Suspense } from 'react'
-import { useAuthContext } from '@/src/app/context/auth'
-import { firebaseApp } from '@/src/app/firebase'
-
-export type OperationForReservationType = {
-  id: string
-  created_at: string
-  latitude: number
-  longitude: number
-}
+import React, { FC, Suspense } from 'react'
+import { useReservations } from './useReservations'
 
 export const OperationForReservationList: FC = () => {
   const router = useRouter()
-  const authContext = useAuthContext()
-  const [reservations, setReservations] =
-    useState<OperationForReservationType[]>()
-  useEffect(() => {
-    const collectionName = 'operation_for_reservations'
-
-    if (authContext.user?.uid) {
-      const db = getFirestore(firebaseApp)
-
-      const compareDate = new Date(Date.now() - 24 * 60 * 60 * 1000)
-      const q = query(
-        collection(db, collectionName),
-        orderBy('created_at', 'desc'),
-        limit(5),
-        where('created_at', '>', compareDate),
-      )
-      const unsubscribe = onSnapshot(q, (querySnapshot) => {
-        const items: OperationForReservationType[] = []
-        querySnapshot.forEach((doc) => {
-          items.push({
-            id: doc.id,
-            latitude: doc.data().latitude,
-            longitude: doc.data().longitude,
-            created_at: doc.data().created_at.toDate().toLocaleString(),
-          })
-        })
-        setReservations(items)
-      })
-      return unsubscribe
-    }
-  }, [authContext.user?.uid])
+  const { reservations } = useReservations()
 
   return (
     <>
