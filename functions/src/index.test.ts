73a06f4a963e6e77572f6272a21f5a1fda1636bb
HiroshiOyamada <h5y1m141+github@gmail.com>
2023-08-31 12:51:53 +0900

    GitHub ActionsでLintとTest実行されるように設定

--- functions/src/index.test.ts
+++ functions/src/index.test.ts
@@ -3,13 +3,7 @@ import functions from 'firebase-functions-test'
 import { copyReservationToOperation } from './index'
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  databaseURL: `https://${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.firebaseio.com`,
+  projectId: 'local-test-project',
 }
 
 const firebaseTest = functions(
