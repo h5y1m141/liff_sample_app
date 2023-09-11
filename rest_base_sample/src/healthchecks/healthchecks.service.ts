740b0a28d0e265b70dab9033d0cc4587733acf1a
HiroshiOyamada <h5y1m141+github@gmail.com>
2023-09-12 08:45:04 +0900

    参照されないライブラリの記述が残っていたので削除

--- rest_base_sample/src/healthchecks/healthchecks.service.ts
+++ rest_base_sample/src/healthchecks/healthchecks.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@nestjs/common'
-import { CreateHealthcheckDto } from './dto/create-healthcheck.dto'
-import { UpdateHealthcheckDto } from './dto/update-healthcheck.dto'
 
 @Injectable()
 export class HealthchecksService {
