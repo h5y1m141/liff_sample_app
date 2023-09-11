acbbe0aefa489a30066fb27d28b3eb880a691e2a
HiroshiOyamada <h5y1m141+github@gmail.com>
2023-09-11 16:17:32 +0900

    一時的にAuthのMiddlewareを停止した処理を戻す

--- rest_base_sample/src/app.module.ts
+++ rest_base_sample/src/app.module.ts
@@ -2,7 +2,7 @@ import {
   MiddlewareConsumer,
   Module,
   NestModule,
-  // RequestMethod,
+  RequestMethod,
 } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
@@ -10,7 +10,7 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { RestaurantsModule } from './restaurants/restaurants.module'
 import { SeatsModule } from './seats/seats.module'
 import { ConfigModule } from '@nestjs/config'
-// import { AuthMiddleware } from './middlewares/AuthMiddleware'
+import { AuthMiddleware } from './middlewares/AuthMiddleware'
 import { ReservationsModule } from './reservations/reservations.module'
 import { TypeOrmConfigService } from './orm.config'
 import { PaymentIntentsModule } from './payment_intents/payment_intents.module'
@@ -35,12 +35,12 @@ import { RestaurantCoursesModule } from './restaurant_courses/restaurant_courses
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    // consumer
-    //   .apply(AuthMiddleware)
-    //   .exclude(
-    //     { path: 'restaurants', method: RequestMethod.GET },
-    //     { path: 'restaurants/:id', method: RequestMethod.GET },
-    //   )
-    //   .forRoutes('*')
+    consumer
+      .apply(AuthMiddleware)
+      .exclude(
+        { path: 'restaurants', method: RequestMethod.GET },
+        { path: 'restaurants/:id', method: RequestMethod.GET },
+      )
+      .forRoutes('*')
   }
 }
