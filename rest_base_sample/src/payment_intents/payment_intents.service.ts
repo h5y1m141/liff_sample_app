1410e7c26baf0e35ca43a500f018eca1137eca56
HiroshiOyamada <h5y1m141+github@gmail.com>
2023-09-12 15:43:58 +0900

    テストコード含めPaymentIntentのServiceクラスのロジック見直し

--- rest_base_sample/src/payment_intents/payment_intents.service.ts
+++ rest_base_sample/src/payment_intents/payment_intents.service.ts
@@ -9,18 +9,15 @@ import Stripe from 'stripe'
 
 @Injectable()
 export class PaymentIntentsService {
-  constructor(private dataSource: DataSource) {}
+  constructor(private dataSource: DataSource, private stripe: Stripe) {}
 
   async create(createPaymentIntentDto: CreatePaymentIntentDto) {
-    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-      apiVersion: '2023-08-16',
-    })
-
     const paymentIntent: Stripe.PaymentIntent =
-      await stripe.paymentIntents.create({
+      await this.stripe.paymentIntents.create({
         amount: createPaymentIntentDto.price,
         currency: 'jpy',
       })
+
     if (paymentIntent) {
       const queryRunner = this.dataSource.createQueryRunner()
       await queryRunner.connect()
@@ -73,21 +70,5 @@ export class PaymentIntentsService {
     } else {
       return { secretKey: null, result: null, seat: null }
     }
-  }
-
-  findAll() {
-    return `This action returns all paymentIntents`
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} paymentIntent`
-  }
-
-  update(id: number, updatePaymentIntentDto: UpdatePaymentIntentDto) {
-    return `This action updates a #${id} paymentIntent`
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} paymentIntent`
   }
 }
